5115c537133dbb453c35fed9dd35322e984549ec
MalikAltawati <Malik.abdullah.abraheem@gmail.com>
2020-02-14 21:26:46 +0100

    done

--- app.js
+++ app.js
@@ -17,11 +17,10 @@ app.use(cookieParser());
 app.use(express.json());
 
 app.use(function (req, res, next) {
-  // res.header("Access-Control-Allow-Origin", "http://localhost:4200");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "*, X-Requested-With, Content-Type, Accept"
-  );
+  res.header("Access-Control-Allow-Origin", "https://unit-is-online.herokuapp.com"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+  //
   next();
 });
 
