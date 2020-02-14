6b4d230808a67a8a4d970dd6f68dad7fac06a4bb
MalikAltawati <Malik.abdullah.abraheem@gmail.com>
2020-02-14 21:23:31 +0100

    done

--- app.js
+++ app.js
@@ -17,11 +17,11 @@ app.use(cookieParser());
 app.use(express.json());
 
 app.use(function (req, res, next) {
-  res.header(Access - Control - Allow - Origin, "*");
-  // res.header(
-  //   "Access-Control-Allow-Headers",
-  //   "Origin, X-Requested-With, Content-Type, Accept"
-  // );
+  // res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "*, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
