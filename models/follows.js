f701ea17fcf1b2b64bcbb3b9a9fa3fa2f33bc779
Abdlrrahman Shibani <56532938+Abdlrrahman@users.noreply.github.com>
2020-02-15 16:51:22 +0100

    Update follows.js

--- models/follows.js
+++ models/follows.js
@@ -12,12 +12,9 @@ conn.query(followsSchema, (err, data) => {
     else console.log("follows table is working")
 })
 
-/*
-select u.id, u.name, u.username, u.photo, p.post, p.link , p.type , p.created_at
-  from users As u JOIN posts AS p ON u.id = p.user_id;
-
-*/
 //User functionality
+
+
 function getfollowersInfo() {
     return conn.query(`select users.id,users.email, users.name, users.username, users.photo, follows.follower_id from users  JOIN follows on followed_id = users.id;`)
 }
@@ -45,6 +42,5 @@ function unfollow(follower_id, followed_id) {
 module.exports.getfollowres = getfollowers;
 module.exports.follow = follow;
 module.exports.unfollow = unfollow;
-
 module.exports.getfollowersInfo = getfollowersInfo;
 module.exports.getfollowingList = getfollowingList;
