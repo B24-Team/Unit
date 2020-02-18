4120848bb33e6794664d2df5fa33c1ebc4de66e1
zied barhoumi <56113259+ziedbarhoumi1989@users.noreply.github.com>
2020-02-18 10:37:56 +0100

    update chatroom

--- models/mogooseModels/chatRoom.js
+++ models/mogooseModels/chatRoom.js
@@ -1,3 +1,4 @@
+// this is the model for the chat romm;the chatroom is identified by a name,messages and the date is was created at
 const db = require("../../db/dbMongo");
 const mongoose = require("mongoose");
 let ChatRoomSchema = mongoose.Schema({
